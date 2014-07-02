f16d1b1497d12a60e8aad10564d19792a4f3b366
Jung-Hoon <Jung-Hoon@Jung-Hoonui-MacBook-Pro.local>
2014-07-02 22:02:49 +0900

    Initial Commit

---
+++ AudioREC_Rev2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AudioREC_Rev2
+//
+//  Created by Jung-Hoon on 2014. 7. 2..
+//  Copyright (c) 2014년 Hoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AudioREC_Rev2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AudioREC_Rev2
+//
+//  Created by Jung-Hoon on 2014. 7. 2..
+//  Copyright (c) 2014년 Hoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
